e40f25b18bba3e6c249a5090c204b87459d936b8
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-07 22:38:45 +0200

    Seperate out delta

---
+++ delta.go
@@ -0,0 +1,35 @@
+package diff
+
+type Delta int
+
+const (
+	// Element is present in both Left and Right collections.
+	// Index uses the Left collection.
+	Both Delta = iota
+	// Element is present only in the Left collection.
+	// Index uses the Left collection.
+	Left
+	// Element is present only in the Right collection.
+	// Index uses the Right collection.
+	Right
+)
+
+func (delta Delta) String() string {
+	switch delta {
+	case Both:
+		return "Both"
+	case Left:
+		return "Left"
+	case Right:
+		return "Right"
+	}
+	return "unknown"
+}
+
+// One Diff record per element.
+// If Delta is Left or Both, Index is for the left collection.
+// If Delta is Right, Index is for the right collection.
+type Diff struct {
+	Delta Delta
+	Index int
+}
--- interface.go
+++ interface.go
@@ -8,37 +8,3 @@ type Interface interface {
 	// Return the sizes of the Left and Right collections.
 	Length() (left int, right int)
 }
-
-type Delta int
-
-const (
-	// Element is present in both Left and Right collections.
-	// Index uses the Left collection.
-	Both Delta = iota
-	// Element is present only in the Left collection.
-	// Index uses the Left collection.
-	Left
-	// Element is present only in the Right collection.
-	// Index uses the Right collection.
-	Right
-)
-
-func (delta Delta) String() string {
-	switch delta {
-	case Both:
-		return "Both"
-	case Left:
-		return "Left"
-	case Right:
-		return "Right"
-	}
-	return "unknown"
-}
-
-// One Diff record per element.
-// If Delta is Left or Both, Index is for the left collection.
-// If Delta is Right, Index is for the right collection.
-type Diff struct {
-	Delta Delta
-	Index int
-}
