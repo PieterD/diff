bbcf71105c6fa8706984e211200615bcc6e2ab12
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-07 19:29:36 +0200

    Add benchmarks

---
+++ bench_test.go
@@ -0,0 +1,42 @@
+package diff_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/PieterD/diff"
+)
+
+func genStr(size int, ratio float64) []byte {
+	b := make([]byte, size)
+	for i := range b {
+		c := byte('a')
+		if rand.Float64() < ratio {
+			c = 'b'
+		}
+		b[i] = c
+	}
+	return b
+}
+
+func strPart(b []byte) []byte {
+	i := rand.Int31n(int32(len(b)))
+	j := i + rand.Int31n(int32(len(b))-i)
+	return b[i:j]
+}
+
+func BenchmarkSame(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		diff.New(Bytes{[]byte("ababaaabababbababbaabbaabababaa"), []byte("aaababbbababaabababababbabababababababababababbabbbabababaaabbb")})
+	}
+}
+
+var str = genStr(500, 0.5)
+
+func BenchmarkRandom(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		a := strPart(str)
+		b := strPart(str)
+		diff.New(Bytes{a, b})
+	}
+}
